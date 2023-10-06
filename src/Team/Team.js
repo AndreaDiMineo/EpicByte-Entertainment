22d0a48a72c43e5059d9c49a385a62d9b89ba476
AndreaDiMineo <117356538+AndreaDiMineo@users.noreply.github.com>
2023-10-06 14:39:41 +0200

    Create Team.js

---
+++ src/Team/Team.js
@@ -0,0 +1,10 @@
+import { Header } from '../Header/Header';
+
+export const Team = () => {
+    return(
+        <>
+            <Header />
+            <label>Team Works!</label>
+        </>
+    )
+}
