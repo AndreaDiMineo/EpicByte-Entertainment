85052a922871e55e8af8a0a7bed61cdc085fd0d3
AndreaDiMineo <117356538+AndreaDiMineo@users.noreply.github.com>
2023-10-06 14:39:03 +0200

    Update Contacts.js

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -1,1 +1,11 @@
+import { Header } from '../Header/Header';
 
+export const Contacts = () => {
+    return(
+        <>
+            <Header />
+            <label>Contacts Works!</label>
+        </>
+    )
+    
+}
