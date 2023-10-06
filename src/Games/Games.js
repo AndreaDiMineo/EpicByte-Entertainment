afea9007d705c45ab70ac21f0b6e539128dccff6
AndreaDiMineo <117356538+AndreaDiMineo@users.noreply.github.com>
2023-10-06 14:36:54 +0200

    Create Games.js

---
+++ src/Games/Games.js
@@ -0,0 +1,11 @@
+
+import { Header } from '../Header/Header';
+
+export const Games = () => {
+    return(
+        <>
+            <Header />
+            <label>Games Works!</label>
+        </>
+    )
+}
