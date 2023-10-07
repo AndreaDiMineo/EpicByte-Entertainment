64c2c2fc6b2b0a72f829f463b13a0675fef29a40
plata03 <131856380+plata03@users.noreply.github.com>
2023-10-07 14:51:14 +0200

    Update Contacts.jsx

--- src/Contacts/Contacts.jsx
+++ src/Contacts/Contacts.jsx
@@ -1,13 +1,38 @@
-import Header from '../Header/Header.jsx';
+import { Header } from '../Header/Header';
+import { Footer } from '../Footer/Footer';
+import "./contactsstyle.css"
 
-const Contacts = () => {
+export const Contacts = () => {
     return(
         <>
             <Header />
-            <label>Contacts Works!</label>
+            <h1 className='contact-us'>CONTACT US!</h1>
+            <div className='contacts-section'>
+                <img className='contacts-logo' src="https://i.ibb.co/Wn1cGyJ/phone-call.png" />
+                <div className='contact-texts'>
+                    <p className='contact-label'>+39 334 885 7475 (Platania)</p>
+                    <p className='contact-label'>+39 334 168 6046 (Di Mineo)</p>
+                    <p className='contact-label'>+39 351 883 7087 (Ben Moussa)</p>
+                </div>
+            </div>
+            <div className='contacts-section'>
+                <img className='contacts-logo' src="https://i.ibb.co/drhjLMc/gmail.png" />
+                <div className='contact-texts'>
+                    <p className='contact-label'>a.platania@itsrizzoli.it</p>
+                    <p className='contact-label'>a.dimineo@itsrizzoli.it</p>
+                    <p className='contact-label'>a.benmoussa@itsrizzoli.it</p>
+                </div>
+            </div>
+            <div className='contacts-section'>
+                <img className='contacts-logo' src="https://i.ibb.co/927zwdH/linkedin-Contact.png" />
+                <div className='contact-texts'>
+                    <p className='contact-label'>linkedin.com/in/alessandro-platania-b9158425a</p>
+                    <p className='contact-label'>linkedin.com/in/andrea-di-mineo-958443230/</p>
+                    <p className='contact-label'>linkedin.com/in/benmoussa-amine/</p>
+                </div>
+            </div>
+            <Footer />
         </>
     )
     
 }
-
-export default Contacts
