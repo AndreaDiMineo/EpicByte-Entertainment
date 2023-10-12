35b0e6aaef98885134a875d799d125a7cedbdb97
plata03 <131856380+plata03@users.noreply.github.com>
2023-10-12 12:24:08 +0200

    Update Contacts.jsx

--- src/Contacts/Contacts.jsx
+++ src/Contacts/Contacts.jsx
@@ -7,52 +7,46 @@ const Contacts = () => {
     <div className="background-contacts">
       <Header />
       <h1 className="contact-us">CONTACT US!</h1>
-      <div className="contacts-section">
-        <img
-          className="contacts-logo"
-          src="https://i.ibb.co/Wn1cGyJ/phone-call.png"
-        />
-        <div className="contact-texts">
-          <p className="contact-label">+39 334 885 7475 (Platania)</p>
-          <p className="contact-label">+39 334 168 6046 (Di Mineo)</p>
-          <p className="contact-label">+39 351 883 7087 (Ben Moussa)</p>
+      <div className="contacts-card">
+        <img className="contacts-pfp" src="https://i.ibb.co/34FxwnD/IMG-20220126-WA00001.jpg" />
+        <div className="contacts-list">
+          <div className="contacts-row">
+            <img className="contacts-logo" src="https://i.ibb.co/drhjLMc/gmail.png" />
+            <p className="contacts-p">a.dimineo@itsrizzoli.it</p>
+          </div>
+          <div className="contacts-row">
+          <img className="contacts-logo" src="https://i.ibb.co/Wn1cGyJ/phone-call.png"/>
+            <p className="contacts-p">+39 334 168 6046</p>
+          </div>
+          <div className="contacts-row">
+          <img className="contacts-logo" src="https://i.ibb.co/927zwdH/linkedin-Contact.png"/>
+            <p className="contacts-p">linkedin.com/in/andrea-di-mineo-958443230/</p>
+          </div>
+          <div className="contacts-row">
+          <img className="contacts-logo" src="https://i.ibb.co/ZfFbKq7/discord.png"/>
+            <p className="contacts-p">andrew900_</p>
+          </div>
         </div>
       </div>
-      <div className="contacts-section">
-        <img
-          className="contacts-logo"
-          src="https://i.ibb.co/drhjLMc/gmail.png"
-        />
-        <div className="contact-texts">
-          <p className="contact-label">alessandro2003.platania@gmail.com</p>
-          <p className="contact-label">a.dimineo@itsrizzoli.it</p>
-          <p className="contact-label">a.benmoussa@itsrizzoli.it</p>
-        </div>
-      </div>
-      <div className="contacts-section">
-        <img
-          className="contacts-logo"
-          src="https://i.ibb.co/927zwdH/linkedin-Contact.png"
-        />
-        <div className="contact-texts">
-          <p className="contact-label">
-            linkedin.com/in/alessandro-platania-b9158425a
-          </p>
-          <p className="contact-label">
-            linkedin.com/in/andrea-di-mineo-958443230/
-          </p>
-          <p className="contact-label">linkedin.com/in/benmoussa-amine/</p>
-        </div>
-      </div>
-      <div className="contacts-section">
-        <img
-          className="contacts-logo"
-          src="https://i.ibb.co/ZfFbKq7/discord.png"
-        />
-        <div className="contact-texts">
-          <p className="contact-label">plata.exe</p>
-          <p className="contact-label">andrew900_</p>
-          <p className="contact-label">Amine-07#4049</p>
+      <div className="contacts-card">
+        <img className="contacts-pfp" src="https://i.ibb.co/7CkbJv7/foto-profilo.jpg" />
+        <div className="contacts-list">
+          <div className="contacts-row">
+            <img className="contacts-logo" src="https://i.ibb.co/drhjLMc/gmail.png" />
+            <p className="contacts-p">alessandro2003.platania@gmail.com</p>
+          </div>
+          <div className="contacts-row">
+          <img className="contacts-logo" src="https://i.ibb.co/Wn1cGyJ/phone-call.png"/>
+            <p className="contacts-p">+39 334 885 7475</p>
+          </div>
+          <div className="contacts-row">
+          <img className="contacts-logo" src="https://i.ibb.co/927zwdH/linkedin-Contact.png"/>
+            <p className="contacts-p">linkedin.com/in/alessandro-platania-b9158425a</p>
+          </div>
+          <div className="contacts-row">
+          <img className="contacts-logo" src="https://i.ibb.co/ZfFbKq7/discord.png"/>
+            <p className="contacts-p">plata_exe</p>
+          </div>
         </div>
       </div>
       <Footer />
