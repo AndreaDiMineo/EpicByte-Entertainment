bc73469c26e3667d9fafcc9f2f120f1fcdada38b
Amine BenMoussa <117638885+BenMousaAmine@users.noreply.github.com>
2023-10-13 14:26:22 +0000

    Update Contacts.jsx and contactsstyle.css

--- src/Contacts/Contacts.jsx
+++ src/Contacts/Contacts.jsx
@@ -7,45 +7,81 @@ const Contacts = () => {
     <div className="background-contacts">
       <Header />
       <h1 className="contact-us">CONTACT US!</h1>
-      <div className="contacts-card">
-        <img className="contacts-pfp" src="https://i.ibb.co/34FxwnD/IMG-20220126-WA00001.jpg" />
-        <div className="contacts-list">
-          <div className="contacts-row">
-            <img className="contacts-logo" src="https://i.ibb.co/drhjLMc/gmail.png" />
-            <p className="contacts-p">a.dimineo@itsrizzoli.it</p>
-          </div>
-          <div className="contacts-row">
-          <img className="contacts-logo" src="https://i.ibb.co/Wn1cGyJ/phone-call.png"/>
-            <p className="contacts-p">+39 334 168 6046</p>
-          </div>
-          <div className="contacts-row">
-          <img className="contacts-logo" src="https://i.ibb.co/927zwdH/linkedin-Contact.png"/>
-            <p className="contacts-p">linkedin.com/in/andrea-di-mineo-958443230/</p>
-          </div>
-          <div className="contacts-row">
-          <img className="contacts-logo" src="https://i.ibb.co/ZfFbKq7/discord.png"/>
-            <p className="contacts-p">andrew900_</p>
+      <div className="background-flex">
+        <div className="contacts-card">
+          <img
+            className="contacts-pfp"
+            src="https://i.ibb.co/LJ8ZqsJ/andrea-removebg-preview.png"
+          />
+          <div className="contacts-list">
+            <div className="contacts-row">
+              <img
+                className="contacts-logo"
+                src="https://i.ibb.co/drhjLMc/gmail.png"
+              />
+              <p className="contacts-p">a.dimineo@itsrizzoli.it</p>
+            </div>
+            <div className="contacts-row">
+              <img
+                className="contacts-logo"
+                src="https://i.ibb.co/Wn1cGyJ/phone-call.png"
+              />
+              <p className="contacts-p">+39 334 168 6046</p>
+            </div>
+            <div className="contacts-row">
+              <img
+                className="contacts-logo"
+                src="https://i.ibb.co/927zwdH/linkedin-Contact.png"
+              />
+              <p className="contacts-p">
+                linkedin.com/in/andrea-di-mineo-958443230/
+              </p>
+            </div>
+            <div className="contacts-row">
+              <img
+                className="contacts-logo"
+                src="https://i.ibb.co/ZfFbKq7/discord.png"
+              />
+              <p className="contacts-p">andrew900_</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="contacts-card">
-        <img className="contacts-pfp" src="https://i.ibb.co/7CkbJv7/foto-profilo.jpg" />
-        <div className="contacts-list">
-          <div className="contacts-row">
-            <img className="contacts-logo" src="https://i.ibb.co/drhjLMc/gmail.png" />
-            <p className="contacts-p">alessandro2003.platania@gmail.com</p>
-          </div>
-          <div className="contacts-row">
-          <img className="contacts-logo" src="https://i.ibb.co/Wn1cGyJ/phone-call.png"/>
-            <p className="contacts-p">+39 334 885 7475</p>
-          </div>
-          <div className="contacts-row">
-          <img className="contacts-logo" src="https://i.ibb.co/927zwdH/linkedin-Contact.png"/>
-            <p className="contacts-p">linkedin.com/in/alessandro-platania-b9158425a</p>
-          </div>
-          <div className="contacts-row">
-          <img className="contacts-logo" src="https://i.ibb.co/ZfFbKq7/discord.png"/>
-            <p className="contacts-p">plata_exe</p>
+        <div className="contacts-card">
+          <img
+            className="contacts-pfp"
+            src="https://i.ibb.co/XCnTT68/alessandro-removebg-preview.png"
+          />
+          <div className="contacts-list">
+            <div className="contacts-row">
+              <img
+                className="contacts-logo"
+                src="https://i.ibb.co/drhjLMc/gmail.png"
+              />
+              <p className="contacts-p">alessandro2003.platania@gmail.com</p>
+            </div>
+            <div className="contacts-row">
+              <img
+                className="contacts-logo"
+                src="https://i.ibb.co/Wn1cGyJ/phone-call.png"
+              />
+              <p className="contacts-p">+39 334 885 7475</p>
+            </div>
+            <div className="contacts-row">
+              <img
+                className="contacts-logo"
+                src="https://i.ibb.co/927zwdH/linkedin-Contact.png"
+              />
+              <p className="contacts-p">
+                linkedin.com/in/alessandro-platania-b9158425a
+              </p>
+            </div>
+            <div className="contacts-row">
+              <img
+                className="contacts-logo"
+                src="https://i.ibb.co/ZfFbKq7/discord.png"
+              />
+              <p className="contacts-p">plata_exe</p>
+            </div>
           </div>
         </div>
       </div>
