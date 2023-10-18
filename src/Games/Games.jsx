c837e6db6e2393d4db598a39c8d0209d2c87494c
plata03 <131856380+plata03@users.noreply.github.com>
2023-10-18 10:26:45 +0200

    Update Games.jsx

--- src/Games/Games.jsx
+++ src/Games/Games.jsx
@@ -1,39 +1,29 @@
-import Header from '../Header/Header.jsx';
-import {Footer} from '../Footer/Footer.jsx';
-import './gamesstyle.css'
+import Header from "../Header/Header.jsx";
+import { Footer } from "../Footer/Footer.jsx";
+import "./gamesstyle.css";
 
 const Games = () => {
-    return(
-        <>
-            <Header />
-            <h1 className='games-title'>OUR GAMES</h1>
-            <div className='games-card'>
-                <img className='games-images' src='https://i.ibb.co/jJKqV0H/unreal-engine-wallpaper-1920x1080.jpg' width={550} height={300}/>
-                <div className='games-side'>
-                    <h2 className='games-game-title'>Title</h2>
-                    <p className='games-by'>by name</p>
-                    <button className='view'>View</button>
-                </div>
-            </div>
-            <div className='games-card'>
-                <img className='games-images' src='https://i.ibb.co/jJKqV0H/unreal-engine-wallpaper-1920x1080.jpg' width={550} height={300} />
-                <div className='games-side'>
-                    <h2 className='games-game-title'>Title</h2>
-                    <p className='games-by'>by name</p>
-                    <button className='view'>View</button>
-                </div>
-            </div>
-            <div className='games-card'>
-                <img className='games-images' src='https://i.ibb.co/jJKqV0H/unreal-engine-wallpaper-1920x1080.jpg' width={550} height={300}/>
-                <div className='games-side'>
-                    <h2 className='games-game-title'>Title</h2>
-                    <p className='games-by'>by name</p>
-                    <button className='view'>View</button>
-                </div>
-            </div>
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      <h1 className="games-title">OUR GAMES</h1>
+      <div className="Game-Conatiner">
+        <div className="games-card">
+          <img
+            src="https://i.ibb.co/jJKqV0H/unreal-engine-wallpaper-1920x1080.jpg"
+            width={300}
+            height={300}
+          />
+          <div className="games-side">
+            <h2 className="games-game-title"> Title </h2>
+            <p className="made-by"> owner </p>
+            <button> View </button>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default Games
+export default Games;
