472ade49fd3d356f787363b43296e44514b3f65f
plata03 <131856380+plata03@users.noreply.github.com>
2023-10-19 11:57:01 +0200

    Update Games.jsx

--- src/Games/Games.jsx
+++ src/Games/Games.jsx
@@ -10,16 +10,31 @@ const Games = () => {
       <div className="Game-Conatiner">
         <div className="games-card">
           <img
+            src="https://i.ibb.co/3pZzBcY/blackout-New.png"
+            width={300}
+            height={300}
+          />
+          <div className="games-side">
+            <h2 className="games-game-title"> Blackout </h2>
+            <p className="made-by"> by <br/> Alessandro Platania </p>
+            <button className="view-button"> View </button>
+          </div>
+        </div>
+      </div>
+      <div className="Game-Conatiner">
+        <div className="games-card">
+          <img
             src="https://i.ibb.co/jJKqV0H/unreal-engine-wallpaper-1920x1080.jpg"
             width={300}
             height={300}
           />
           <div className="games-side">
             <h2 className="games-game-title"> Title </h2>
-            <p className="made-by"> owner </p>
-            <button> View </button>
+            <p className="made-by"> by <br/> TBC </p>
+            <button className="view-button"> View </button>
           </div>
         </div>
+        
       </div>
       <Footer />
     </>
